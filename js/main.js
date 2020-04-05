e7f40b5d5cb029fb9abc1e90106afeb42725412f
lalyperra <35471457+lalyperra@users.noreply.github.com>
2020-04-04 18:35:11 -0700

    add more less for formatting, add API code part 1, working the navigation bar and ad pop-up

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the image and insert it inside the modal 
-var img = document.getElementById("adImg");
+var img = document.getElementById("adImage");
 var modalImg = document.getElementById("img01");
 img.onclick = function () {
     modal.style.display = "block";
@@ -38,3 +40,54 @@ document.querySelector('.show-more-button').addEventListener('click', function (
         this.previousSibling.previousSibling.style.display = 'none';
     }
 });
+
+/*Downloaded from https://www.codeseek.co/SgiobairOg/responsive-multi-level-dropdown-vanilla-js-evzmdN */
+window.addEventListener("load", function load(event) {
+    window.removeEventListener("load", load, false); 
+    navInit();
+}, false);
+
+var navInit = function () {
+    var
+        nav = document.querySelector('nav#main'),
+        menuToggleBtns = nav.querySelectorAll('.toggle'),
+        menuToggle = function (e) {
+            //Do not visit menuToggle link
+            e.preventDefault();
+
+            //force submenus closed
+            if (e.target.hasAttribute('data-toggle-main')) {
+                e.target.parentNode.querySelectorAll('ul li.open').forEach(function (tog) {
+                    tog.classList.remove('open');
+                });
+            }
+
+            //Toggle 'menuToggle' .focus class
+            e.target.parentNode.classList.toggle('open');
+        };
+
+
+    //add touchstart and click event handlers
+    menuToggleBtns.forEach(function (tog) {
+        tog.addEventListener('click', menuToggle)
+        tog.addEventListener('touchstart', menuToggle)
+    });
+};
+
+
+//API for charging stations
+var chargeLocation = document.getElementById('chargeLocation');
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var apiResult = JSON.parse(this.responseText);
+
+        //print weather description after main section text <p>
+        var chargingStations = document.createTextNode(apiResult.AddressInfo[0].title);
+        chargeLocation.appendChild(chargingStations);
+    }
+};
+
+xmlhttp.open('GET', 'https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=10', true);
+xmlhttp.send();
